8ac4e1b01c94b4a2af9019e93a30f284f4a69049
Aartisarode <65185113+Aartisarode@users.noreply.github.com>
2022-03-20 14:38:05 +0530

    Add files via upload

---
+++ Assignmrnt6/Assignment6/src/com/main/addBook.java
@@ -0,0 +1,50 @@
+package com.main;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class addBook {
+	public static void main(String[] args) {
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Driver loaded");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		PreparedStatement stmt=null;
+		Connection connection=null;
+		
+		try {
+			 connection=DriverManager.getConnection
+					("jdbc:mysql://localhost:3306/bookstore", "book", "book");
+			
+			stmt=connection.
+					prepareStatement("insert into book(book_id, book_name, book_author, book_price) values(?,?,?,?)");
+			
+			stmt.setInt(1, 1004);
+			stmt.setString(2, "Shivaji The Warrior");
+			stmt.setString(3, "Ranjit Desai");
+			stmt.setInt(4, 694);
+			
+			int noOfRowsEffected= stmt.executeUpdate();
+			System.out.println(noOfRowsEffected);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			if(connection!=null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		
+	}
+
+
+}
